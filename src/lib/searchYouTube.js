886da2640cb40a8c33b51968e9a29a549e589091
Hack Reactor Students <accounts@hackreactor.com>
2017-01-06 21:08:10 -0800

    Add partial searchYouTube functionality

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var params = options;
+
+  var optionQuery = () => {
+    return '?key=' + options.key + '&q=' + options.query + '&maxResults=' + options.max; 
+  };
+
+  $.ajax({
+    //https://developers.google.com/youtube/v3/getting-started
+    url: 'https://www.googleapis.com/youtube/v3/search' + optionQuery(),
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      callback();
+      console.log(data, 'successfully receieved data');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('all bad');
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
